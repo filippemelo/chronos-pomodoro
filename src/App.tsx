09fef958233f04d8432dc115bd61f901f850ef8a
Filippe Melo <filippemelor@gmail.com>
2026-02-25 14:30:04 -0300

    Criando componente Footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import { CountDown } from './components/CountDown'
 import { Cycles } from './components/Cycles'
 import { DefaultInput } from './components/DefaultInput'
 import { DefaultButton } from './components/DefaultButton'
-import { PlayCircleIcon, StopCircleIcon } from 'lucide-react'
+import { PlayCircleIcon } from 'lucide-react'
+import { Footer } from './components/Footer'
 
 import './styles/theme.css'
 import './styles/global.css'
+
 
 export function App() {
     return (
@@ -41,9 +43,12 @@ export function App() {
 
                     <div className='formRow'>
                         <DefaultButton icon={<PlayCircleIcon />} color='green' />
-                        <DefaultButton icon={<StopCircleIcon />} color='red' />
                     </div>
                 </form>
+            </Container>
+
+            <Container>
+                <Footer />
             </Container>
         </>
     );
