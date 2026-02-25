b79a18e4d627ed9d9e802c3fb65f06459650e094
Filippe Melo <filippemelor@gmail.com>
2026-02-24 23:07:27 -0300

    Criando componente de Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,46 @@
+import { Container } from './components/Container'
+import { Logo } from './components/Logo'
+import { Menu } from './components/Menu'
+import { CountDown } from './components/CountDown'
+import { Cycles } from './components/Cycles'
+
 import './styles/theme.css'
 import './styles/global.css'
-
-import { Container } from './components/Container'
+import { DefaultInput } from './components/DefaultInput'
 
 export function App() {
     return (
         <>
             <Container>
-                <section>LOGO</section>
+                <Logo />
             </Container>
 
             <Container>
-                <section>MENU</section>
+                <Menu />
+            </Container>
+
+            <Container>
+                <CountDown />
+            </Container>
+
+            <Container>
+                <form className='form' action=''>
+                    <div className='formRow'>
+                        <DefaultInput id='meuInput' labelText='Task' type='text' placeholder='Digite algo' />
+                    </div>
+
+                    <div className='formRow'>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+
+                    <div className='formRow'>
+                        <Cycles />
+                    </div>
+
+                    <div className='formRow'>
+                        <button>Enviar</button>
+                    </div>
+                </form>
             </Container>
         </>
     );
